0eef0eb9b3c47559eab77e3ccbbc009f625ee91b
larissaalmeidalira <larissaaloliveira2003@gmail.com>
2025-03-10 10:14:46 -0300

    Classes criadas, classe HomeController será a principal. Adicionando Dashboard-API

---
+++ Dashboard-API/Dashboard-API/Model/Entregas.cs
@@ -0,0 +1,13 @@
+﻿namespace Dashboard_API.Model
+{
+    public class Entregas
+    {
+        public int idEntrega { get; set; }
+        public int usuarioId { get; set; } // Chave estrangeira -> USUÁRIO
+        public Usuario usuario { get; set; }
+        public int atividadeId { get; set; } // Chave estrangeira -> ATIVIDADE
+        public Atividade atividade { get; set; }
+        public string arquivoAnexo { get; set; }
+        public DateTime dataEntrega { get; set; }
+    }
+}
