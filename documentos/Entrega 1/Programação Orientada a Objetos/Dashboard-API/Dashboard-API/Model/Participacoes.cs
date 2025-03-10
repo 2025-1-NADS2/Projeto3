a8df55bc94bf189d96bdf49301717a74941a7a9d
larissaalmeidalira <larissaaloliveira2003@gmail.com>
2025-03-10 10:19:45 -0300

    Copiando projeto para pasta de entrega. Classes criadas, classe principal HomeController

---
+++ documentos/Entrega 1/Programação Orientada a Objetos/Dashboard-API/Dashboard-API/Model/Participacoes.cs
@@ -0,0 +1,13 @@
+﻿namespace Dashboard_API.Model
+{
+    public class Participacoes
+    {
+        public int idParticipacoes { get; set; }
+        public int usuarioId { get; set; } // Chave estrangeira -> Usuario
+        public Usuario usuario { get; set; }
+        public int projetoId { get; set; } // Chave estrangeira -> Projeto
+        public Projeto projeto { get; set; }
+        public double progresso { get; set; }
+        public DateTime dataEntrada { get; set; }
+    }
+}
