a92fea096756cfdd2c67597d8794b0b29b598d23
larissaalmeidalira <larissaaloliveira2003@gmail.com>
2025-03-11 13:02:21 -0300

    back-end

---
+++ Dashboard-API/Dashboard-API/Model/Participacao.cs
@@ -0,0 +1,15 @@
+﻿namespace Dashboard_API.Model
+{
+    public class Participacao
+    {
+        public int IdParticipacao { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public int EventoId { get; set; }
+        public Evento Evento { get; set; }
+
+        public string TokenIngresso { get; set; } // Token único gerado para controle de presença
+        public bool Presenca { get; set; } // Será atualizado após leitura do token
+    }
+}
---
+++ Dashboard-API/Dashboard-API/Model/Usuario.cs
@@ -0,0 +1,29 @@
+﻿namespace Dashboard_API.Model
+{
+    public class Usuario
+    {
+        public int idUsuario { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string? Profissao { get; set; }
+
+        // chama a enum com os tipos de usuários
+        public TipoUsuario tipoUsuario { get; set; }
+
+
+        // relacionamento de um para muitos (1:N)
+        public ICollection<Evento> Eventos { get; set; }
+        public ICollection<Participacao> Participacoes { get; set; }
+
+    }
+
+    // Enum que define os tipos de usuários
+    public enum TipoUsuario
+    {
+        Administrador = 1,
+        Colaborador = 2,
+        UsuarioComum = 3
+    }
+}
