54c128bee77f0447f3f27446fb8615c1668c4544
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2025-03-11 21:30:20 -0300

    Update FotosEventos.cs

--- documentos/Entrega 1/Programação Orientada a Objetos/FotosEventos.cs
+++ documentos/Entrega 1/Programação Orientada a Objetos/FotosEventos.cs
@@ -2,10 +2,52 @@ namespace Dashboard_API.Model
 {
     public class FotosEventos
     {
-        public int IdFotos { get; set; }
-        public int EventoId { get; set; }
-        public Evento Evento { get; set; }
+        public int IdFotos //{ get; set; }
+        public int EventoId //{ get; set; }
+        public Evento Evento //{ get; set; }
 
-        public string UrlImagem { get; set; }
+        public string UrlImagem //{ get; set; }
+
+
+        public int GetIdFotos()
+    {
+        return IdFotos;
+    }
+    public void SetIdFotos(int idFotos)
+    {
+        IdFotos = idFotos;
+    }
+
+    // Get e Set para EventoId
+    public int GetEventoId()
+    {
+        return EventoId;
+    }
+    public void SetEventoId(int eventoId)
+    {
+        EventoId = eventoId;
+    }
+
+    // Get e Set para Evento
+    public Evento GetEvento()
+    {
+        return Evento;
+    }
+    public void SetEvento(Evento evento)
+    {
+        Evento = evento;
+    }
+
+    // Get e Set para UrlImagem
+    public string GetUrlImagem()
+    {
+        return UrlImagem;
+    }
+    public void SetUrlImagem(string urlImagem)
+    {
+        UrlImagem = urlImagem;
+    }
+
+        
     }
 }
