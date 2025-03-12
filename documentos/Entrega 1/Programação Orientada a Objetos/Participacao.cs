7fd24e16c5e8945b41ff383131ffb9d4cb4d649c
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2025-03-11 21:32:35 -0300

    Update Participacao.cs

--- documentos/Entrega 1/Programação Orientada a Objetos/Participacao.cs
+++ documentos/Entrega 1/Programação Orientada a Objetos/Participacao.cs
@@ -2,14 +2,87 @@ namespace Dashboard_API.Model
 {
     public class Participacao
     {
-        public int IdParticipacao { get; set; }
-        public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
+        public int IdParticipacao;
+        public int UsuarioId; 
+        public Usuario Usuario; 
 
-        public int EventoId { get; set; }
-        public Evento Evento { get; set; }
+        public int EventoId; 
+        public Evento Evento; 
 
-        public string TokenIngresso { get; set; } // Token único gerado para controle de presença
-        public bool Presenca { get; set; } // Será atualizado após leitura do token
+        public string TokenIngresso;  // Token único gerado para controle de presença
+        public bool Presenca;  // Será atualizado após leitura do token
+
+
+    // Get e Set para IdParticipacao
+    public int GetIdParticipacao()
+    {
+        return IdParticipacao;
+    }
+    public void SetIdParticipacao(int idParticipacao)
+    {
+        IdParticipacao = idParticipacao;
+    }
+
+    // Get e Set para UsuarioId
+    public int GetUsuarioId()
+    {
+        return UsuarioId;
+    }
+    public void SetUsuarioId(int usuarioId)
+    {
+        UsuarioId = usuarioId;
+    }
+
+    // Get e Set para Usuario
+    public Usuario GetUsuario()
+    {
+        return Usuario;
+    }
+    public void SetUsuario(Usuario usuario)
+    {
+        Usuario = usuario;
+    }
+
+    // Get e Set para EventoId
+    public int GetEventoId()
+    {
+        return EventoId;
+    }
+    public void SetEventoId(int eventoId)
+    {
+        EventoId = eventoId;
+    }
+
+    // Get e Set para Evento
+    public Evento GetEvento()
+    {
+        return Evento;
+    }
+    public void SetEvento(Evento evento)
+    {
+        Evento = evento;
+    }
+
+    // Get e Set para TokenIngresso
+    public string GetTokenIngresso()
+    {
+        return TokenIngresso;
+    }
+    public void SetTokenIngresso(string tokenIngresso)
+    {
+        TokenIngresso = tokenIngresso;
+    }
+
+    // Get e Set para Presenca
+    public bool GetPresenca()
+    {
+        return Presenca;
+    }
+    public void SetPresenca(bool presenca)
+    {
+        Presenca = presenca;
+    }
+
+        
     }
 }
