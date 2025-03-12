93c73aff2e1903f9c3f7c3b39e9e5aed8a14c9fd
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2025-03-11 21:23:52 -0300

    Update Evento.cs

--- documentos/Entrega 1/Programação Orientada a Objetos/Evento.cs
+++ documentos/Entrega 1/Programação Orientada a Objetos/Evento.cs
@@ -22,6 +22,163 @@ namespace Dashboard_API.Model
         // relacionamento de um para muitos (1:N)
         public ICollection<Participacao> Participacoes { get; set; }
         public ICollection<FotosEventos> Fotos { get; set; }
+
+
+          // Gets e Sets 
+        public int GetIdEvento()
+        {
+            return IdEvento;
+        }
+        public void SetIdEvento(int idEvento)
+        {
+            IdEvento = idEvento;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string GetObjetivo()
+        {
+            return Objetivo;
+        }
+        public void SetObjetivo(string objetivo)
+        {
+            Objetivo = objetivo;
+        }
+
+        public string GetPalavrasChave()
+        {
+            return PalavrasChave;
+        }
+        public void SetPalavrasChave(string palavrasChave)
+        {
+            PalavrasChave = palavrasChave;
+        }
+
+        public DateTime GetDataInicio()
+        {
+            return DataInicio;
+        }
+        public void SetDataInicio(DateTime dataInicio)
+        {
+            DataInicio = dataInicio;
+        }
+
+        public TimeSpan GetHoraInicio()
+        {
+            return HoraInicio;
+        }
+        public void SetHoraInicio(TimeSpan horaInicio)
+        {
+           HoraInicio = horaInicio;
+        }
+
+        public DateTime GetDataFinal()
+        {
+            return DataFinal;
+        }
+        public void SetDataFinal(DateTime dataFinal)
+        {
+           DataFinal = dataFinal;
+        }
+
+        public TimeSpan GetHoraFinal()
+        {
+            return HoraFinal;
+        }
+        public void SetHoraFinal(TimeSpan horaFinal)
+        {
+            HoraFinal = horaFinal;
+        }
+
+        public string? GetEndereco()
+        {
+            return Endereco;
+        }
+        public void SetEndereco(string? endereco)
+        {
+            Endereco= endereco;
+        }
+
+        public ModalidadeEvento GetModalidade()
+        {
+            return Modalidade;
+        }
+        public void SetModalidade(ModalidadeEvento modalidade)
+        {
+            Modalidade = modalidade;
+        }
+
+        public int GetVagas()
+        {
+            return Vagas;
+        }
+        public void SetVagas(int vagas)
+        {
+            Vagas = vagas;
+        }
+
+        public int GetTotalInscritos()
+        {
+            return TotalInscritos;
+        }
+        public void SetTotalInscritos(int totalInscritos)
+        {
+            TotalInscritos = totalInscritos;
+        }
+
+        public StatusEvento GetStatus()
+        {
+            return Status;
+        }
+        public void SetStatus(StatusEvento status)
+        {
+            Status = status;
+        }
+
+        public int GetColaboradorId()
+        {
+            return ColaboradorId;
+        }
+        public void SetColaboradorId(int colaboradorId)
+        {
+            ColaboradorId = colaboradorId;
+        }
+
+        public Usuario GetColaborador()
+        {
+            return Colaborador;
+        }
+        public void SetColaborador(Usuario colaborador)
+        {
+            Colaborador = colaborador;
+        }
+
+        public ICollection<Participacao> GetParticipacoes()
+        {
+            return Participacoes;
+        }
+        public void SetParticipacoes(ICollection<Participacao> participacoes)
+        {
+            Participacoes = participacoes;
+        }
+
+        public ICollection<FotosEventos> GetFotos()
+        {
+            return Fotos;
+        }
+        public void SetFotos(ICollection<FotosEventos> fotos)
+        {
+            Fotos = fotos;
+        }
+
+        
     }
 
     public enum ModalidadeEvento
